153d864e75959728fc2e8a38d7a1929a6c7fc395
TAzazi09 <162177648+TAzazi09@users.noreply.github.com>
2024-07-17 15:55:08 +0100

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,59 +1,65 @@
 import React, { Component } from 'react';
 import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
-import { DataManipulator } from './DataManipulator';
 import './Graph.css';
 
 interface IProps {
-  data: ServerRespond[],
+  data: ServerRespond[];
+  ratio: { timestamp: Date, value: number }[];
+  upperBound: number;
+  lowerBound: number;
 }
 
 interface PerspectiveViewerElement extends HTMLElement {
-  load: (table: Table) => void,
+  load: (table: Table) => void;
 }
+
 class Graph extends Component<IProps, {}> {
   table: Table | undefined;
 
-  render() {
-    return React.createElement('perspective-viewer');
-  }
-
   componentDidMount() {
-    // Get element from the DOM.
-    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
       timestamp: 'date',
+      ratio: 'float',
+      upperBound: 'float',
+      lowerBound: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
+
     if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('column-pivots', '[""]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "upperBound", "lowerBound"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
+        ratio: 'avg',
+        upperBound: 'avg',
+        lowerBound: 'avg',
       }));
     }
   }
 
-  componentDidUpdate() {
-    if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+  componentDidUpdate(prevProps: IProps) {
+    if (this.table && this.props.data !== prevProps.data) {
+      const uniqueData = this.props.ratio.map((el) => ({
+        timestamp: el.timestamp,
+        ratio: el.value,
+        upperBound: this.props.upperBound,
+        lowerBound: this.props.lowerBound,
+      }));
+
+      this.table.update(uniqueData);
     }
+  }
+
+  render() {
+    return React.createElement('perspective-viewer');
   }
 }
 
