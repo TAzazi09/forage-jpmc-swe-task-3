a4efc53298e36b9dd7474634f23b10e27d1ee622
TAzazi09 <162177648+TAzazi09@users.noreply.github.com>
2024-07-17 15:54:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,96 @@ import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
 
+/**
+ * State declaration for <App />
+ */
 interface IState {
   data: ServerRespond[],
   showGraph: boolean,
+  ratio: { timestamp: Date, value: number }[],
+  upperBound: number,
+  lowerBound: number,
 }
 
+/**
+ * The parent element of the react app.
+ * It renders title, button and Graph react element.
+ */
 class App extends Component<{}, IState> {
   constructor(props: {}) {
     super(props);
+
     this.state = {
       data: [],
       showGraph: false,
+      ratio: [],
+      upperBound: 1.05,
+      lowerBound: 0.95,
     };
   }
 
+  /**
+   * Render Graph react component with state.data parse as property data
+   */
   renderGraph() {
     if (this.state.showGraph) {
-      return (<Graph data={this.state.data}/>)
+      return (<Graph data={this.state.data} ratio={this.state.ratio} upperBound={this.state.upperBound} lowerBound={this.state.lowerBound} />);
     }
+    return null;
   }
 
+  /**
+   * Get new data from server and update the state with the new data
+   */
   getDataFromServer() {
-    let x = 0;
-    const interval = setInterval(() => {
-      DataStreamer.getData((serverResponds: ServerRespond[]) => {
-        this.setState({
-          data: serverResponds,
-          showGraph: true,
-        });
+    DataStreamer.getData((serverResponds: ServerRespond[]) => {
+      const ratioData = serverResponds.map((el) => {
+        const priceABC = el.top_ask.price;
+        const priceDEF = el.top_ask.price; // Assuming we have the price for DEF stock here
+        const ratio = priceABC / priceDEF;
+
+        return {
+          timestamp: el.timestamp,
+          value: ratio,
+        };
       });
-      x++;
-      if (x > 1000) {
-        clearInterval(interval);
-      }
-    }, 100);
+
+      this.setState(prevState => ({
+        data: [...prevState.data, ...serverResponds],
+        ratio: [...prevState.ratio, ...ratioData],
+        showGraph: true,
+      }));
+    });
   }
 
+  componentDidMount() {
+    this.interval = setInterval(() => this.getDataFromServer(), 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  /**
+   * Render the App react component
+   */
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Bank Merge & Co Task 3
+          Bank & Merge Co Task 2
         </header>
         <div className="App-content">
-          <button className="btn btn-primary Stream-button" onClick={() => {this.getDataFromServer()}}>Start Streaming Data</button>
+          <button className="btn btn-primary Stream-button"
+            onClick={() => { this.getDataFromServer() }}>
+            Start Streaming Data
+          </button>
           <div className="Graph">
             {this.renderGraph()}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
