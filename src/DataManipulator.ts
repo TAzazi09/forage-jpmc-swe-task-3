9608afa26d3f9460d5f8bb28ef9da939139067ca
TAzazi09 <162177648+TAzazi09@users.noreply.github.com>
2024-07-17 16:04:05 +0100

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,13 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
   timestamp: Date,
+  ratio: number,
+  upperBound: number,
+  lowerBound: number,
+  trigger_alert: number | undefined,
 }
-
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
-  }
-}
+  static generateRow(serverResponds: ServerRespond[]): Row[] {
+    const priceABC = serverResponds.filter((stock) => stock
