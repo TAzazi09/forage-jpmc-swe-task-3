111fe9b98cf74b5ff87dd08dbec8df88fa06f950
TAzazi09 <162177648+TAzazi09@users.noreply.github.com>
2024-07-17 16:04:41 +0100

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -10,4 +10,26 @@ export interface Row {
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]): Row[] {
-    const priceABC = serverResponds.filter((stock) => stock
+    const priceABC = serverResponds.filter((stock) => stock.stock === 'ABC')[0]?.top_ask?.price;
+    const priceDEF = serverResponds.filter((stock) => stock.stock === 'DEF')[0]?.top_bid?.price;
+
+    if (!priceABC || !priceDEF) {
+      return [];
+    }
+
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1.1;
+    const lowerBound = 0.9;
+    const trigger_alert = ratio > upperBound || ratio < lowerBound ? ratio : undefined;
+
+    return serverResponds.map((el: ServerRespond) => {
+      return {
+        timestamp: el.timestamp,
+        ratio: ratio,
+        upperBound: upperBound,
+        lowerBound: lowerBound,
+        trigger_alert: trigger_alert,
+      };
+    });
+  }
+}
